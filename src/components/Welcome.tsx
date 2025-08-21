317b55e83d965ebe07284eafebbd6aea2917099b
Laboratório FIAP <caiofelipesilva832@gmail.com>
2025-08-20 22:18:41 -0300

    feat: criando o componente welcome e TENTANDO estlizar

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,15 @@
+export function Welcome() {
+    return(
+
+    <header> 
+        <div style={{background:"azure"}}>
+        <title>TrackFit MIni</title>
+        <h1 style={{color:"blue"}}>Seja bem vindo ao TrackfitMini</h1>
+        <p>O sucesso no treino não é sobre ser o melhor, mas sobre ser melhor do que você era ontem.</p>
+        <div>Olá visitante  &#x1F604;</div>
+        </div>
+    </header>
+    
+    )
+
+}
